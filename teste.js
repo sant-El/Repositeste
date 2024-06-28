9d186ac57f024dafd7aa76847c1dcf2ad978c193
sant-El <eliasvsr1@gmail.com>
2024-06-27 21:18:08 -0300

    editar

---
+++ teste.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+let igual = 0;
+let contadorIgual = 0;
+let a = 1
+
+let entradaBruta = prompt("Conte uma história: ");
+
+let entrada = entradaBruta.split(/[.,!? :\-?()+=]/).filter(Boolean);
+
+let tamanhoHistoria = entrada.length;
+
+for (let x = 0 ; x <= entrada.length ; x++) {
+if () {
+    
+}else {
+    
+}
+
+
+}
+
+
+console.log(`Sua história contém ${tamanhoHistoria} palavras.`);
+console.log(`Existem ${igual} palavras iguais.`);
